f3daae107f616f52b8f01742338d82456bc16b3d
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-10-27 21:21:05 +0400

    One more step towards prototype of KLLE

--- src/edrt/libedrt.cpp
+++ src/edrt/libedrt.cpp
@@ -9,24 +9,102 @@
 #include <string.h>
 
 #include <vector>
+#include <algorithm>
 
 #include "../neighbor/CoverTree.hpp"
 #include <eigen3/Eigen/SparseCore>
+#include <eigen3/Eigen/Dense>
 
 typedef std::vector<int> LocalNeighbors;
 typedef std::vector<LocalNeighbors> Neighbors;
 typedef Eigen::SparseMatrix<double> WeightMatrix;
 typedef Eigen::MatrixXd EmbeddingMatrix;
 
-template <class ForwardIterator, class PairwiseCallback>
-Neighbors find_neighbors(ForwardIterator begin, ForwardIterator end, PairwiseCallback callback, unsigned int k)
+template <class RandomAccessIterator, class PairwiseCallback>
+Neighbors find_neighbors(RandomAccessIterator begin, RandomAccessIterator end, PairwiseCallback callback, unsigned int k)
 {
+	typedef std::pair<RandomAccessIterator, double> DistanceRecord;
+	typedef std::vector<DistanceRecord> Distances;
+	
+	struct distances_comparator
+	{
+		bool operator()(const DistanceRecord& l, const DistanceRecord& r) 
+		{
+			return (l.second < r.second);
+		}
+	};
+
+	Neighbors neighbors;
+	neighbors.reserve(end-begin);
+	Distances distances;
+	distances.reserve(end-begin);
+	for (RandomAccessIterator iter=begin; iter!=end; ++iter)
+	{
+		distances.clear();
+		for (RandomAccessIterator around_iter=begin; around_iter!=end; ++around_iter)
+			distances.push_back(make_pair(around_iter, callback(iter,around_iter)));
+		
+		std::partial_sort(distances.begin(),distances.begin()+k+1,distances.end(),distances_comparator());
+
+		LocalNeighbors local_neighbors;
+		local_neighbors.reserve(k);
+		for (typename DistanceRecord::const_iterator neighbors_iter=distances.begin()+1; 
+				neighbors_iter!=distances.begin()+k+1; ++neighbors_iter)
+			local_neighbors.push_back(neighbors_iter->first - begin);
+		neighbors.push_back(local_neighbors);
+	}
+
+	return neighbors;
 }
 
-template <class ForwardIterator, class PairwiseCallback>
-WeightMatrix klle_weight_matrix(ForwardIterator begin, ForwardIterator end, PairwiseCallback callback)
+template <class RandomAccessIterator, class PairwiseCallback>
+WeightMatrix klle_weight_matrix(RandomAccessIterator begin, RandomAccessIterator end, Neighbors neighbors, PairwiseCallback callback)
 {
+	typedef Eigen::Triplet<double> SparseTriplet;
+	typedef std::vector<SparseTriplet> SparseTriplets;
 
+	SparseTriplets sparse_triplets;
+
+	int k = neighbors[0].size();
+	for (RandomAccessIterator iter=begin; iter!=end; ++iter)
+	{
+		double kernel_value = callback(iter,iter);
+		LocalNeighbors& current_neighbors = neighbors[iter-begin];
+		
+		Eigen::VectorXf dots(k);
+		for (int i=0; i<k; ++i)
+			dots[i] = callback(iter, begin+current_neighbors[i]);
+
+		Eigen::MatrixXd gram_matrix = Eigen::MatrixXd::Zero(k,k);
+		gram_matrix += kernel_value;
+		for (int i=0; i<k; ++i)
+		{
+			gram_matrix.row(i) -= dots(i);
+			gram_matrix.col(i) -= dots(i);
+			for (int j=0; j<k; ++j)
+				gram_matrix(i,j) -= callback(begin+i,begin+j);
+		}
+		gram_matrix.diagonal() += 1e-3*gram_matrix.trace();
+		Eigen::VectorXd rhs = Eigen::VectorXd::Ones(k);
+		Eigen::VectorXd weights = gram_matrix.ldlt().solve(rhs);
+		weights /= weights.sum();
+
+		sparse_triplets.push_back(SparseTriplet(iter-begin,iter-begin,1.0));
+		for (int i=0; i<k; ++i)
+		{
+			sparse_triplets.push_back(SparseTriplet(current_neighbors[i],iter-begin,
+			                                        -weights[i]));
+			sparse_triplets.push_back(SparseTriplet(iter-begin,current_neighbors[i],
+			                                        -weights[i]));
+			for (int j=0; j<k; ++j)
+				sparse_triplets.push_back(SparseTriplet(current_neighbors[i],current_neighbors[j],
+				                                        +gram_matrix(i,j)));
+		}
+	}
+
+	WeightMatrix weight_matrix(begin-end,begin-end);
+	weight_matrix.setFromTriplets(sparse_triplets.begin(),sparse_triplets.end());
+	return weight_matrix;
 }
 
 EmbeddingMatrix eigen_embedding(WeightMatrix wm, unsigned int target_dimension)
@@ -34,10 +112,10 @@ EmbeddingMatrix eigen_embedding(WeightMatrix wm, unsigned int target_dimension)
 
 }
 
-template <class ForwardIterator, class PairwiseCallback, class ResultInsertIterator>
+template <class RandomAccessIterator, class PairwiseCallback, class ResultInsertIterator>
 int embed(
-		ForwardIterator begin,
-		ForwardIterator end,
+		RandomAccessIterator begin,
+		RandomAccessIterator end,
 		const edrt_options_t& options,
 		const int target_dimension,
 		const int N,
