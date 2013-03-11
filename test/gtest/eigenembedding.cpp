f2d8a6c3ce3e00188bfa36e3199aa0fecf3354e9
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-03-11 10:16:35 +0400

    Older Eigen3 support for eigenembedding tests

--- test/gtest/eigenembedding.cpp
+++ test/gtest/eigenembedding.cpp
@@ -32,11 +32,21 @@ TEST(EigenEmbedding, EigenDenseLargestEigenvector)
 
 TEST(EigenEmbedding, EigenSparseSmallestEigenvector) 
 {
-	tapkee::SparseWeightMatrix mat(3,3);
-	tapkee::tapkee_internal::SparseTriplets triplets;
-	for (int i=0; i<3; i++)
-		triplets.push_back(tapkee::tapkee_internal::SparseTriplet(i,i,tapkee::ScalarType(i+1)));
-	mat.setFromTriplets(triplets.begin(), triplets.end());
+	const int N = 3;
+	tapkee::tapkee_internal::SparseTriplets sparse_triplets;
+	for (int i=0; i<N; i++)
+		sparse_triplets.push_back(tapkee::tapkee_internal::SparseTriplet(i,i,tapkee::ScalarType(i+1)));
+
+#ifdef EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
+	Eigen::DynamicSparseMatrix<ScalarType> dynamic_weight_matrix(N,N);
+	dynamic_weight_matrix.reserve(sparse_triplets.size());
+	for (SparseTriplets::const_iterator it=sparse_triplets.begin(); it!=sparse_triplets.end(); ++it)
+		dynamic_weight_matrix.coeffRef(it->col(),it->row()) += it->value();
+	tapkee::SparseWeightMatrix mat(dynamic_weight_matrix);
+#else
+	tapkee::SparseWeightMatrix mat(N,N);
+	mat.setFromTriplets(sparse_triplets.begin(),sparse_triplets.end());
+#endif
 
 	tapkee::tapkee_internal::EmbeddingResult result = 
 		tapkee::tapkee_internal::eigen_embedding<tapkee::SparseWeightMatrix,tapkee::tapkee_internal::SparseInverseMatrixOperation>
@@ -71,11 +81,21 @@ TEST(EigenEmbedding, ArpackDenseLargestEigenvector)
 
 TEST(EigenEmbedding, ArpackSparseSmallestEigenvector) 
 {
-	tapkee::SparseWeightMatrix mat(3,3);
-	tapkee::tapkee_internal::SparseTriplets triplets;
-	for (int i=0; i<3; i++)
-		triplets.push_back(tapkee::tapkee_internal::SparseTriplet(i,i,tapkee::ScalarType(i+1)));
-	mat.setFromTriplets(triplets.begin(), triplets.end());
+	const int N = 3;
+	tapkee::tapkee_internal::SparseTriplets sparse_triplets;
+	for (int i=0; i<N; i++)
+		sparse_triplets.push_back(tapkee::tapkee_internal::SparseTriplet(i,i,tapkee::ScalarType(i+1)));
+
+#ifdef EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
+	Eigen::DynamicSparseMatrix<ScalarType> dynamic_weight_matrix(N,N);
+	dynamic_weight_matrix.reserve(sparse_triplets.size());
+	for (SparseTriplets::const_iterator it=sparse_triplets.begin(); it!=sparse_triplets.end(); ++it)
+		dynamic_weight_matrix.coeffRef(it->col(),it->row()) += it->value();
+	tapkee::SparseWeightMatrix mat(dynamic_weight_matrix);
+#else
+	tapkee::SparseWeightMatrix mat(N,N);
+	mat.setFromTriplets(sparse_triplets.begin(),sparse_triplets.end());
+#endif
 
 	tapkee::tapkee_internal::EmbeddingResult result = 
 		tapkee::tapkee_internal::eigen_embedding<tapkee::SparseWeightMatrix,tapkee::tapkee_internal::SparseInverseMatrixOperation>
