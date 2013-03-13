683642b37e8c80e938c87aa70723ae3386bacf44
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-03-13 13:50:05 +0400

    Update generate_swissroll.py

--- test/generate_swissroll.py
+++ test/generate_swissroll.py
@@ -4,12 +4,12 @@ tt = numpy.array((3*numpy.pi/2)*(1+2*numpy.random.rand(N)))
 height = numpy.array((numpy.random.rand(N)-0.5))
 X = numpy.array([tt*numpy.cos(tt), 10*height, tt*numpy.sin(tt)])
 
-from pylab import *
-from mpl_toolkits.mplot3d import Axes3D
-fig = figure()
-ax = fig.add_subplot(111,projection='3d')
-ax.scatter(X[0],X[1],X[2],c=tt,cmap=cm.Spectral)
-show()
+#from pylab import *
+#from mpl_toolkits.mplot3d import Axes3D
+#fig = figure()
+#ax = fig.add_subplot(111,projection='3d')
+#ax.scatter(X[0],X[1],X[2],c=tt,cmap=cm.Spectral)
+#show()
 
 numpy.savetxt('colormap.dat',tt)
 numpy.savetxt('input.dat',X.T)
