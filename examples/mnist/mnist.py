d789aedd9e15156d91cf954b42a210163b86b53c
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2018-04-26 16:44:02 +0300

    Update mnist example

--- examples/mnist/mnist.py
+++ examples/mnist/mnist.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy, datetime, json, subprocess, sys, os
 
 def embed(fname):
@@ -6,13 +7,13 @@ def embed(fname):
 		json_data = json.load(file)
 	N, d = json_data['N'], json_data['d']
 	X = numpy.zeros((N,d))
-	for i in xrange(N):
+	for i in range(N):
 		X[i,json_data['data'][str(i)]] = 1.0
-	numpy.savetxt(input_file,X.T,fmt='%.1f')
+	numpy.savetxt(input_file, X.T, fmt='%.1f', delimiter=',')
 	output_file = 'tmp_mnist_output.dat'
-	run_string = './bin/tapkee_cli -i %s -o %s -m t-sne --verbose --benchmark' % (input_file,output_file)
+	run_string = './bin/tapkee -i %s -o %s -m t-sne --verbose --benchmark' % (input_file,output_file)
 	output = subprocess.check_output(run_string, shell=True)
-	embedding = numpy.loadtxt(output_file)
+	embedding = numpy.loadtxt(output_file, delimiter=',').T
 	os.remove(input_file)
 	os.remove(output_file)
 	return embedding, X
@@ -23,7 +24,7 @@ def plot_embedding(embedding,data):
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	ax.scatter(embedding[0],embedding[1],alpha=0.0)
-	for i in xrange(embedding.shape[1]):
+	for i in range(embedding.shape[1]):
 		img = numpy.zeros((28,28,4))
 		img[:,:,0] = 255*data[i,:].reshape(28,28)
 		img[:,:,1] = 255*data[i,:].reshape(28,28)
@@ -40,16 +41,16 @@ def plot_embedding(embedding,data):
 def export_json(outfile,embedding,data):
 	json_dict = {}
 	N = embedding.shape[1]
-	print 'N', N
+	print('N', N)
 	import scipy.misc
-	for i in xrange(N):
+	for i in range(N):
 		img = numpy.zeros((28,28,4))
 		img[:,:,0] = 255*data[i,:].reshape(28,28)
 		img[:,:,1] = 255*data[i,:].reshape(28,28)
 		img[:,:,2] = 255*data[i,:].reshape(28,28)
 		img[:,:,3] = 255*data[i,:].reshape(28,28)
 		scipy.misc.imsave('mnist/%d.png' % i,img)
-	json_dict['data'] = [{'cx':embedding[0,i], 'cy':embedding[1,i], 'fname':'%d.png' % i} for i in xrange(N)]
+	json_dict['data'] = [{'cx':embedding[0,i], 'cy':embedding[1,i], 'fname':'%d.png' % i} for i in range(N)]
 	json.dump(json_dict, open(outfile, 'w'))
 
 if __name__ == "__main__":
