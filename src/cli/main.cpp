41356df3d8e776dca6eb89a9488ae9e36b66735b
Sergey Lisitsyn <lisitsyn@hey.com>
2023-04-15 22:59:47 +0400

    Use macro function for safer prefixing

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -48,37 +48,41 @@ int run(int argc, const char **argv)
     opt.syntax = "tapkee [options]\n";
 
 #if defined(USE_SLASH_CLI_WINDOWS) && (defined(_WIN32) || defined(_WIN64))
-#define OPT_PREFIX "/"
-#define OPT_LONG_PREFIX "/"
+#define __OPT_PREFIX "/"
+#define __OPT_LONG_PREFIX "/"
 #else
-#define OPT_PREFIX "-"
-#define OPT_LONG_PREFIX "--"
+#define __OPT_PREFIX "-"
+#define __OPT_LONG_PREFIX "--"
 #endif
+#define OPT_PREFIXED(X) \
+    __OPT_PREFIX X
+#define OPT_LONG_PREFIXED(X) \
+    __OPT_LONG_PREFIX X
 
 #define INPUT_FILE_KEYWORD "input-file"
-    opt.add("", 0, 1, 0, "Input file", OPT_PREFIX "i", OPT_LONG_PREFIX INPUT_FILE_KEYWORD);
+    opt.add("", 0, 1, 0, "Input file", OPT_PREFIXED("i"), OPT_LONG_PREFIXED(INPUT_FILE_KEYWORD));
 #define TRANSPOSE_INPUT_KEYWORD "transpose-input"
-    opt.add("", 0, 0, 0, "Transpose input file if set", OPT_LONG_PREFIX TRANSPOSE_INPUT_KEYWORD);
+    opt.add("", 0, 0, 0, "Transpose input file if set", OPT_LONG_PREFIXED(TRANSPOSE_INPUT_KEYWORD));
 #define TRANSPOSE_OUTPUT_KEYWORD "transpose-output"
-    opt.add("", 0, 0, 0, "Transpose output file if set", OPT_LONG_PREFIX TRANSPOSE_OUTPUT_KEYWORD);
+    opt.add("", 0, 0, 0, "Transpose output file if set", OPT_LONG_PREFIXED(TRANSPOSE_OUTPUT_KEYWORD));
 #define OUTPUT_FILE_KEYWORD "output-file"
-    opt.add("", 0, 1, 0, "Output file", OPT_PREFIX "o", OPT_LONG_PREFIX OUTPUT_FILE_KEYWORD);
+    opt.add("", 0, 1, 0, "Output file", OPT_PREFIXED("o"), OPT_LONG_PREFIXED(OUTPUT_FILE_KEYWORD));
 #define OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD "output-projection-matrix-file"
-    opt.add("", 0, 1, 0, "Output file for projection matrix", OPT_PREFIX "opmat",
-            OPT_LONG_PREFIX OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD);
+    opt.add("", 0, 1, 0, "Output file for projection matrix", OPT_PREFIXED("opmat"),
+            OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD));
 #define OUTPUT_PROJECTION_MEAN_FILE_KEYWORD "output-projection-mean-file"
-    opt.add("", 0, 1, 0, "Output file for mean of data", OPT_PREFIX "opmean",
-            OPT_LONG_PREFIX OUTPUT_PROJECTION_MEAN_FILE_KEYWORD);
+    opt.add("", 0, 1, 0, "Output file for mean of data", OPT_PREFIXED("opmean"),
+            OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MEAN_FILE_KEYWORD));
 #define DELIMITER_KEYWORD "delimiter"
-    opt.add("", 0, 1, 0, "Delimiter", OPT_PREFIX "d", OPT_LONG_PREFIX DELIMITER_KEYWORD);
+    opt.add("", 0, 1, 0, "Delimiter", OPT_PREFIXED("d"), OPT_LONG_PREFIXED(DELIMITER_KEYWORD));
 #define HELP_KEYWORD "help"
-    opt.add("", 0, 0, 0, "Display help", OPT_PREFIX "h", OPT_LONG_PREFIX HELP_KEYWORD);
+    opt.add("", 0, 0, 0, "Display help", OPT_PREFIXED("h"), OPT_LONG_PREFIXED(HELP_KEYWORD));
 #define BENCHMARK_KEYWORD "benchmark"
-    opt.add("", 0, 0, 0, "Output benchmark information", OPT_LONG_PREFIX BENCHMARK_KEYWORD);
+    opt.add("", 0, 0, 0, "Output benchmark information", OPT_LONG_PREFIXED(BENCHMARK_KEYWORD));
 #define VERBOSE_KEYWORD "verbose"
-    opt.add("", 0, 0, 0, "Output more information", OPT_LONG_PREFIX VERBOSE_KEYWORD);
+    opt.add("", 0, 0, 0, "Output more information", OPT_LONG_PREFIXED(VERBOSE_KEYWORD));
 #define DEBUG_KEYWORD "debug"
-    opt.add("", 0, 0, 0, "Output debug information", OPT_LONG_PREFIX DEBUG_KEYWORD);
+    opt.add("", 0, 0, 0, "Output debug information", OPT_LONG_PREFIXED(DEBUG_KEYWORD));
 #define METHOD_KEYWORD "method"
     opt.add(
         "locally_linear_embedding", 0, 1, 0,
@@ -90,7 +94,7 @@ int run(int argc, const char **argv)
         "landmark_multidimensional_scaling (l-mds), stochastic_proximity_embedding (spe), \n"
         "kernel_pca (kpca), pca, random_projection (ra), factor_analysis (fa), \n"
         "t-stochastic_neighborhood_embedding (t-sne), manifold_sculpting (ms).",
-        OPT_PREFIX "m", OPT_LONG_PREFIX METHOD_KEYWORD);
+        OPT_PREFIXED("m"), OPT_LONG_PREFIXED(METHOD_KEYWORD));
 #define NEIGHBORS_METHOD_KEYWORD "neighbors-method"
     opt.add(
 #ifdef TAPKEE_USE_LGPL_COVERTREE
@@ -105,7 +109,7 @@ int run(int argc, const char **argv)
         ",covertree"
 #endif
         ".",
-        OPT_PREFIX "nm", OPT_LONG_PREFIX NEIGHBORS_METHOD_KEYWORD);
+        OPT_PREFIXED("nm"), OPT_LONG_PREFIXED(NEIGHBORS_METHOD_KEYWORD));
 #define EIGEN_METHOD_KEYWORD "eigen-method"
     opt.add(
 #ifdef TAPKEE_WITH_ARPACK
@@ -119,7 +123,7 @@ int run(int argc, const char **argv)
         "arpack, "
 #endif
         "randomized, dense.",
-        OPT_PREFIX "em", OPT_LONG_PREFIX EIGEN_METHOD_KEYWORD);
+        OPT_PREFIXED("em"), OPT_LONG_PREFIXED(EIGEN_METHOD_KEYWORD));
 #define COMPUTATION_STRATEGY_KEYWORD "computation-strategy"
     opt.add("cpu", 0, 1, 0,
             "Computation strategy (default is 'cpu'). One of the following: "
@@ -127,44 +131,44 @@ int run(int argc, const char **argv)
             "opencl, "
 #endif
             "cpu.",
-            OPT_PREFIX "cs", OPT_LONG_PREFIX COMPUTATION_STRATEGY_KEYWORD);
+            OPT_PREFIXED("cs"), OPT_LONG_PREFIXED(COMPUTATION_STRATEGY_KEYWORD));
 #define TARGET_DIMENSION_KEYWORD "target-dimension"
-    opt.add("2", 0, 1, 0, "Target dimension (default 2)", OPT_PREFIX "td", OPT_LONG_PREFIX TARGET_DIMENSION_KEYWORD);
+    opt.add("2", 0, 1, 0, "Target dimension (default 2)", OPT_PREFIXED("td"), OPT_LONG_PREFIXED(TARGET_DIMENSION_KEYWORD));
 #define NUM_NEIGHBORS_KEYWORD "num-neighbors"
-    opt.add("10", 0, 1, 0, "Number of neighbors (default 10)", OPT_PREFIX "k", OPT_LONG_PREFIX NUM_NEIGHBORS_KEYWORD);
+    opt.add("10", 0, 1, 0, "Number of neighbors (default 10)", OPT_PREFIXED("k"), OPT_LONG_PREFIXED(NUM_NEIGHBORS_KEYWORD));
 #define GAUSSIAN_WIDTH_KEYWORD "gaussian-width"
-    opt.add("1.0", 0, 1, 0, "Width of gaussian kernel (default 1.0)", OPT_PREFIX "gw",
-            OPT_LONG_PREFIX GAUSSIAN_WIDTH_KEYWORD);
+    opt.add("1.0", 0, 1, 0, "Width of gaussian kernel (default 1.0)", OPT_PREFIXED("gw"),
+            OPT_LONG_PREFIXED(GAUSSIAN_WIDTH_KEYWORD));
 #define TIMESTEPS_KEYWORD "timesteps"
-    opt.add("1", 0, 1, 0, "Number of timesteps for diffusion map (default 1)", OPT_LONG_PREFIX TIMESTEPS_KEYWORD);
+    opt.add("1", 0, 1, 0, "Number of timesteps for diffusion map (default 1)", OPT_LONG_PREFIXED(TIMESTEPS_KEYWORD));
 #define SPE_LOCAL_KEYWORD "spe-local"
-    opt.add("0", 0, 0, 0, "Local strategy in SPE (default global)", OPT_LONG_PREFIX SPE_LOCAL_KEYWORD);
+    opt.add("0", 0, 0, 0, "Local strategy in SPE (default global)", OPT_LONG_PREFIXED(SPE_LOCAL_KEYWORD));
 #define EIGENSHIFT_KEYWORD "eigenshift"
     opt.add("1e-9", 0, 1, 0, "Regularization diagonal shift for weight matrix (default 1e-9)",
-            OPT_LONG_PREFIX EIGENSHIFT_KEYWORD);
+            OPT_LONG_PREFIXED(EIGENSHIFT_KEYWORD));
 #define LANDMARK_RATIO_KEYWORD "landmark-ratio"
     opt.add("0.2", 0, 1, 0, "Ratio of landmarks. Should be in (0,1) range (default 0.2, i.e. 20%)",
-            OPT_LONG_PREFIX LANDMARK_RATIO_KEYWORD);
+            OPT_LONG_PREFIXED(LANDMARK_RATIO_KEYWORD));
 #define SPE_TOLERANCE_KEYWORD "spe-tolerance"
-    opt.add("1e-5", 0, 1, 0, "Tolerance for SPE (default 1e-5)", OPT_LONG_PREFIX SPE_TOLERANCE_KEYWORD);
+    opt.add("1e-5", 0, 1, 0, "Tolerance for SPE (default 1e-5)", OPT_LONG_PREFIXED(SPE_TOLERANCE_KEYWORD));
 #define SPE_NUM_UPDATES_KEYWORD "spe-num-updates"
-    opt.add("100", 0, 1, 0, "Number of SPE updates (default 100)", OPT_LONG_PREFIX SPE_NUM_UPDATES_KEYWORD);
+    opt.add("100", 0, 1, 0, "Number of SPE updates (default 100)", OPT_LONG_PREFIXED(SPE_NUM_UPDATES_KEYWORD));
 #define MAX_ITERS_KEYWORD "max-iters"
-    opt.add("1000", 0, 1, 0, "Maximum number of iterations (default 1000)", OPT_LONG_PREFIX MAX_ITERS_KEYWORD);
+    opt.add("1000", 0, 1, 0, "Maximum number of iterations (default 1000)", OPT_LONG_PREFIXED(MAX_ITERS_KEYWORD));
 #define FA_EPSILON_KEYWORD "fa-epsilon"
-    opt.add("1e-5", 0, 1, 0, "FA convergence criterion (default 1e-5)", OPT_LONG_PREFIX FA_EPSILON_KEYWORD);
+    opt.add("1e-5", 0, 1, 0, "FA convergence criterion (default 1e-5)", OPT_LONG_PREFIXED(FA_EPSILON_KEYWORD));
 #define SNE_PERPLEXITY_KEYWORD "sne-perplexity"
     opt.add("30.0", 0, 1, 0, "Perplexity for the t-SNE algorithm (default 30.0)",
-            OPT_LONG_PREFIX SNE_PERPLEXITY_KEYWORD);
+            OPT_LONG_PREFIXED(SNE_PERPLEXITY_KEYWORD));
 #define SNE_THETA_KEYWORD "sne-theta"
-    opt.add("0.5", 0, 1, 0, "Theta for the t-SNE algorithm (default 0.5)", OPT_LONG_PREFIX SNE_THETA_KEYWORD);
+    opt.add("0.5", 0, 1, 0, "Theta for the t-SNE algorithm (default 0.5)", OPT_LONG_PREFIXED(SNE_THETA_KEYWORD));
 #define MS_SQUISHING_RATE_KEYWORD "squishing-rate"
     opt.add("0.99", 0, 1, 0, "Squishing rate of the Manifold Sculpting algorithm (default 0.5)",
-            OPT_LONG_PREFIX MS_SQUISHING_RATE_KEYWORD);
+            OPT_LONG_PREFIXED(MS_SQUISHING_RATE_KEYWORD));
 
     opt.parse(argc, argv);
 
-    if (opt.isSet(OPT_LONG_PREFIX HELP_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(HELP_KEYWORD)))
     {
         string usage;
         opt.getUsage(usage);
@@ -172,17 +176,17 @@ int run(int argc, const char **argv)
         return 1;
     }
 
-    if (opt.isSet(OPT_LONG_PREFIX VERBOSE_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(VERBOSE_KEYWORD)))
     {
         tapkee::LoggingSingleton::instance().enable_info();
     }
-    if (opt.isSet(OPT_LONG_PREFIX DEBUG_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(DEBUG_KEYWORD)))
     {
         tapkee::LoggingSingleton::instance().enable_debug();
         tapkee::LoggingSingleton::instance().message_info("Debug messages enabled");
     }
 
-    if (opt.isSet(OPT_LONG_PREFIX BENCHMARK_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(BENCHMARK_KEYWORD)))
     {
         tapkee::LoggingSingleton::instance().enable_benchmark();
         tapkee::LoggingSingleton::instance().message_info("Benchmarking enabled");
@@ -191,7 +195,7 @@ int run(int argc, const char **argv)
     tapkee::DimensionReductionMethod tapkee_method;
     {
         string method;
-        opt.get(OPT_LONG_PREFIX METHOD_KEYWORD)->getString(method);
+        opt.get(OPT_LONG_PREFIXED(METHOD_KEYWORD))->getString(method);
         try
         {
             tapkee_method = parse_reduction_method(method.c_str());
@@ -206,7 +210,7 @@ int run(int argc, const char **argv)
     tapkee::NeighborsMethod tapkee_neighbors_method = tapkee::Brute;
     {
         string method;
-        opt.get(OPT_LONG_PREFIX NEIGHBORS_METHOD_KEYWORD)->getString(method);
+        opt.get(OPT_LONG_PREFIXED(NEIGHBORS_METHOD_KEYWORD))->getString(method);
         try
         {
             tapkee_neighbors_method = parse_neighbors_method(method.c_str());
@@ -220,7 +224,7 @@ int run(int argc, const char **argv)
     tapkee::EigenMethod tapkee_eigen_method = tapkee::Dense;
     {
         string method;
-        opt.get(OPT_LONG_PREFIX EIGEN_METHOD_KEYWORD)->getString(method);
+        opt.get(OPT_LONG_PREFIXED(EIGEN_METHOD_KEYWORD))->getString(method);
         try
         {
             tapkee_eigen_method = parse_eigen_method(method.c_str());
@@ -234,7 +238,7 @@ int run(int argc, const char **argv)
     tapkee::ComputationStrategy tapkee_computation_strategy = tapkee::HomogeneousCPUStrategy;
     {
         string method;
-        opt.get(OPT_LONG_PREFIX COMPUTATION_STRATEGY_KEYWORD)->getString(method);
+        opt.get(OPT_LONG_PREFIXED(COMPUTATION_STRATEGY_KEYWORD))->getString(method);
         try
         {
             tapkee_computation_strategy = parse_computation_strategy(method.c_str());
@@ -247,7 +251,7 @@ int run(int argc, const char **argv)
     }
     int target_dim = 1;
     {
-        opt.get(OPT_LONG_PREFIX TARGET_DIMENSION_KEYWORD)->getInt(target_dim);
+        opt.get(OPT_LONG_PREFIXED(TARGET_DIMENSION_KEYWORD))->getInt(target_dim);
         if (target_dim < 0)
         {
             tapkee::LoggingSingleton::instance().message_error(
@@ -258,7 +262,7 @@ int run(int argc, const char **argv)
     }
     int k = 1;
     {
-        opt.get(OPT_LONG_PREFIX NUM_NEIGHBORS_KEYWORD)->getInt(k);
+        opt.get(OPT_LONG_PREFIXED(NUM_NEIGHBORS_KEYWORD))->getInt(k);
         if (k < 3)
         {
             tapkee::LoggingSingleton::instance().message_error(
@@ -268,7 +272,7 @@ int run(int argc, const char **argv)
     }
     double width = 1.0;
     {
-        opt.get(OPT_LONG_PREFIX GAUSSIAN_WIDTH_KEYWORD)->getDouble(width);
+        opt.get(OPT_LONG_PREFIXED(GAUSSIAN_WIDTH_KEYWORD))->getDouble(width);
         if (width < 0.0)
         {
             tapkee::LoggingSingleton::instance().message_error("Width of the gaussian kernel is negative.");
@@ -277,7 +281,7 @@ int run(int argc, const char **argv)
     }
     int timesteps = 1;
     {
-        opt.get(OPT_LONG_PREFIX TIMESTEPS_KEYWORD)->getInt(timesteps);
+        opt.get(OPT_LONG_PREFIXED(TIMESTEPS_KEYWORD))->getInt(timesteps);
         if (timesteps < 0)
         {
             tapkee::LoggingSingleton::instance().message_error("Number of timesteps is negative.");
@@ -286,80 +290,80 @@ int run(int argc, const char **argv)
     }
     double eigenshift = 1e-9;
     {
-        opt.get(OPT_LONG_PREFIX EIGENSHIFT_KEYWORD)->getDouble(eigenshift);
+        opt.get(OPT_LONG_PREFIXED(EIGENSHIFT_KEYWORD))->getDouble(eigenshift);
     }
     double landmark_rt = 0.0;
     {
-        opt.get(OPT_LONG_PREFIX LANDMARK_RATIO_KEYWORD)->getDouble(landmark_rt);
+        opt.get(OPT_LONG_PREFIXED(LANDMARK_RATIO_KEYWORD))->getDouble(landmark_rt);
     }
     bool spe_global = false;
     {
-        if (opt.isSet(OPT_LONG_PREFIX SPE_LOCAL_KEYWORD))
+        if (opt.isSet(OPT_LONG_PREFIXED(SPE_LOCAL_KEYWORD)))
             spe_global = false;
         else
             spe_global = true;
     }
     double spe_tol = 1e-5;
     {
-        opt.get(OPT_LONG_PREFIX SPE_TOLERANCE_KEYWORD)->getDouble(spe_tol);
+        opt.get(OPT_LONG_PREFIXED(SPE_TOLERANCE_KEYWORD))->getDouble(spe_tol);
     }
     int spe_num_upd = 100;
     {
-        opt.get(OPT_LONG_PREFIX SPE_NUM_UPDATES_KEYWORD)->getInt(spe_num_upd);
+        opt.get(OPT_LONG_PREFIXED(SPE_NUM_UPDATES_KEYWORD))->getInt(spe_num_upd);
     }
     int max_iters = 1000;
     {
-        opt.get(OPT_LONG_PREFIX MAX_ITERS_KEYWORD)->getInt(max_iters);
+        opt.get(OPT_LONG_PREFIXED(MAX_ITERS_KEYWORD))->getInt(max_iters);
     }
     double fa_eps = 1e-5;
     {
-        opt.get(OPT_LONG_PREFIX FA_EPSILON_KEYWORD)->getDouble(fa_eps);
+        opt.get(OPT_LONG_PREFIXED(FA_EPSILON_KEYWORD))->getDouble(fa_eps);
     }
     double perplexity = 30.0;
     {
-        opt.get(OPT_LONG_PREFIX SNE_PERPLEXITY_KEYWORD)->getDouble(perplexity);
+        opt.get(OPT_LONG_PREFIXED(SNE_PERPLEXITY_KEYWORD))->getDouble(perplexity);
     }
     double theta = 0.5;
     {
-        opt.get(OPT_LONG_PREFIX SNE_THETA_KEYWORD)->getDouble(theta);
+        opt.get(OPT_LONG_PREFIXED(SNE_THETA_KEYWORD))->getDouble(theta);
     }
     double squishing = 0.99;
     {
-        opt.get(OPT_LONG_PREFIX MS_SQUISHING_RATE_KEYWORD)->getDouble(squishing);
+        opt.get(OPT_LONG_PREFIXED(MS_SQUISHING_RATE_KEYWORD))->getDouble(squishing);
     }
 
     // Load data
     string input_filename;
     string output_filename;
-    if (!opt.isSet(OPT_LONG_PREFIX INPUT_FILE_KEYWORD))
+    if (!opt.isSet(OPT_LONG_PREFIXED(INPUT_FILE_KEYWORD)))
     {
         // tapkee::LoggingSingleton::instance().message_warning("No input file specified, using stdin");
         input_filename = "/dev/stdin";
     }
     else
     {
-        opt.get(OPT_LONG_PREFIX INPUT_FILE_KEYWORD)->getString(input_filename);
+        opt.get(OPT_LONG_PREFIXED(INPUT_FILE_KEYWORD))->getString(input_filename);
     }
 
-    if (!opt.isSet(OPT_LONG_PREFIX OUTPUT_FILE_KEYWORD))
+    if (!opt.isSet(OPT_LONG_PREFIXED(OUTPUT_FILE_KEYWORD)))
     {
         // tapkee::LoggingSingleton::instance().message_warning("No output file specified, using stdout");
         output_filename = "/dev/stdout";
     }
     else
     {
-        opt.get(OPT_LONG_PREFIX OUTPUT_FILE_KEYWORD)->getString(output_filename);
+        opt.get(OPT_LONG_PREFIXED(OUTPUT_FILE_KEYWORD))->getString(output_filename);
     }
 
     bool output_projection = false;
     std::string output_matrix_filename = "/dev/null";
     std::string output_mean_filename = "/dev/null";
-    if (opt.isSet(OPT_LONG_PREFIX OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD) &&
-        opt.isSet(OPT_LONG_PREFIX OUTPUT_PROJECTION_MEAN_FILE_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD)) &&
+        opt.isSet(OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MEAN_FILE_KEYWORD)))
     {
         output_projection = true;
-        opt.get(OPT_LONG_PREFIX OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD)->getString(output_matrix_filename);
-        opt.get(OPT_LONG_PREFIX OUTPUT_PROJECTION_MEAN_FILE_KEYWORD)->getString(output_mean_filename);
+        opt.get(OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MATRIX_FILE_KEYWORD))->getString(output_matrix_filename);
+        opt.get(OPT_LONG_PREFIXED(OUTPUT_PROJECTION_MEAN_FILE_KEYWORD))->getString(output_mean_filename);
     }
 
     ifstream ifs(input_filename.c_str());
@@ -368,11 +372,11 @@ int run(int argc, const char **argv)
     ofstream ofs_mean(output_mean_filename.c_str());
 
     std::string delimiter = ",";
-    if (opt.isSet(OPT_LONG_PREFIX DELIMITER_KEYWORD))
-        opt.get(OPT_LONG_PREFIX DELIMITER_KEYWORD)->getString(delimiter);
+    if (opt.isSet(OPT_LONG_PREFIXED(DELIMITER_KEYWORD)))
+        opt.get(OPT_LONG_PREFIXED(DELIMITER_KEYWORD))->getString(delimiter);
 
     tapkee::DenseMatrix input_data = read_data(ifs, delimiter[0]);
-    if (!opt.isSet(OPT_LONG_PREFIX TRANSPOSE_INPUT_KEYWORD))
+    if (!opt.isSet(OPT_LONG_PREFIXED(TRANSPOSE_INPUT_KEYWORD)))
         input_data.transposeInPlace();
 
     std::stringstream ss;
@@ -428,7 +432,7 @@ int run(int argc, const char **argv)
     output = tapkee::initialize().withParameters(parameters).embedUsing(input_data);
 #endif
     // Save obtained data
-    if (opt.isSet(OPT_LONG_PREFIX TRANSPOSE_OUTPUT_KEYWORD))
+    if (opt.isSet(OPT_LONG_PREFIXED(TRANSPOSE_OUTPUT_KEYWORD)))
     {
         output.embedding.transposeInPlace();
     }
@@ -446,8 +450,10 @@ int run(int argc, const char **argv)
     ofs_matrix.close();
     ofs_mean.close();
     return 0;
-#undef OPT_PREFIX
-#undef OPT_LONG_PREFIX
+#undef __OPT_PREFIX
+#undef __OPT_LONG_PREFIX
+#undef OPT_PREFIXED
+#undef OPT_LONG_PREFIXED
 }
 
 int main(int argc, const char **argv)
