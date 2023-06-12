6bf667b274f06cbaa377c1caf5f0fa3dede07699
Sergey Lisitsyn <lisitsyn@hey.com>
2023-06-12 22:44:14 +0400

    Prettify go.py output

--- examples/go.py
+++ examples/go.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import numpy
 import sys
 import os
 import subprocess
+
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-supported_methods = ('lle','ltsa','isomap','mds','pca','kpca','tsne')
+supported_methods = {
+	'lle': 'Locally Linear Embedding',
+	'ltsa': 'Local Tangent Space Alignment',
+	'isomap': 'Isomap',
+	'mds': 'Multidimensional Scaling',
+	'pca': 'Principal Component Analysis',
+	'kpca': 'Kernel Principal Component Analysis',
+	't-sne': 't-distributed Stochastic Neighborhood Embedding',
+	'dm': 'Diffusion Map',
+}
 
 def generate_data(type, N=1000):
 	if type=='swissroll':
-		tt = numpy.array((3*numpy.pi/2)*(1+2*numpy.random.rand(N)))
-		height = numpy.array((numpy.random.rand(N)-0.5))
-		X = numpy.array([tt*numpy.cos(tt), 10*height, tt*numpy.sin(tt)])
+		tt = np.array((3*np.pi/2)*(1+2*np.random.rand(N)))
+		height = np.array((np.random.rand(N)-0.5))
+		X = np.array([tt*np.cos(tt), 10*height, tt*np.sin(tt)])
 		return X,tt
 	if type=='scurve':
-		tt = numpy.array((3*numpy.pi*(numpy.random.rand(N)-0.5)))
-		height = numpy.array((numpy.random.rand(N)-0.5))
-		X = numpy.array([numpy.sin(tt), 10*height, numpy.sign(tt)*(numpy.cos(tt)-1)])
+		tt = np.array((3*np.pi*(np.random.rand(N)-0.5)))
+		height = np.array((np.random.rand(N)-0.5))
+		X = np.array([np.sin(tt), 10*height, np.sign(tt)*(np.cos(tt)-1)])
 		return X,tt
 	if type=='helix':
-		tt = numpy.linspace(1,N,N).T / N
-		tt = tt*2*numpy.pi
-		X = numpy.r_[[(2+numpy.cos(8*tt))*numpy.cos(tt)],
-		             [(2+numpy.cos(8*tt))*numpy.sin(tt)],
-		             [numpy.sin(8*tt)]]
+		tt = np.linspace(1,N,N).T / N
+		tt = tt*2*np.pi
+		X = np.r_[[(2+np.cos(8*tt))*np.cos(tt)],
+		             [(2+np.cos(8*tt))*np.sin(tt)],
+		             [np.sin(8*tt)]]
 		return X,tt
 
 	raise Exception('Dataset is not supported')
@@ -38,32 +48,39 @@ def embed(data,method):
 
 	input_file = 'tapkee_input_data'
 	output_file = 'tapkee_output_data'
-	numpy.savetxt(input_file, data.T,delimiter=',')
+	np.savetxt(input_file, data.T,delimiter=',')
 	tapkee_binary = 'bin/tapkee'
-	runner_string = '%s -i %s -o %s -m %s -k 20 --verbose --transpose-output --benchmark' % (tapkee_binary,input_file,output_file,method)
+	runner_string = '%s -i %s -o %s -m %s -k 20 --verbose --transpose-output --benchmark' % (tapkee_binary, input_file, output_file, method)
 	print('-- To reproduce this use the following command', runner_string)
-	output = subprocess.check_output(runner_string,shell=True)
-	embedded_data = numpy.loadtxt(output_file,delimiter=',')
+	output = subprocess.check_output(runner_string, shell=True)
+	embedded_data = np.loadtxt(output_file, delimiter=',')
 	os.remove(input_file)
 	os.remove(output_file)
 	return embedded_data
 
-def plot(data,embedded_data,colors='m'):
+def plot(data, embedded_data, colors='m', method=None):
 	fig = plt.figure()
 	fig.set_facecolor('white')
-	ax = fig.add_subplot(121,projection='3d')
-	ax.scatter(data[0],data[1],data[2],c=colors,cmap=plt.cm.Spectral)
-	plt.axis('tight'); plt.axis('off')
+
+	ax = fig.add_subplot(121, projection='3d')
+	ax.scatter(data[0], data[1], data[2], c=colors, cmap=plt.cm.Spectral, s=5)
+	plt.axis('tight')
+	plt.axis('off')
+	plt.title('Original', fontsize=9)
+
 	ax = fig.add_subplot(122)
-	ax.scatter(embedded_data[0],embedded_data[1],c=colors,cmap=plt.cm.Spectral)
-	plt.axis('tight'); plt.axis('off')
+	ax.scatter(embedded_data[0], embedded_data[1], c=colors, cmap=plt.cm.Spectral, s=5)
+	plt.axis('tight')
+	plt.axis('off')
+	plt.title('Embedding' + (' with ' + method) if method else '', fontsize=9)
+
 	plt.show()
 
 if __name__ == "__main__":
 	import argparse
 	parser = argparse.ArgumentParser(description='Graphical example of dimension reduction with Tapkee.')
-	parser.add_argument('dataset',type=str,nargs=1,help='A dataset to embed. One of the following: swissroll, scurve, helix.')
-	parser.add_argument('method',type=str,nargs=1,help='A method to use. One of the following %s' % str(supported_methods))
+	parser.add_argument('dataset', type=str, nargs=1, help='A dataset to embed. One of the following: %s' % str(['swissroll', 'scurve', 'helix']))
+	parser.add_argument('method', type=str, nargs=1, help='A method to use. One of the following %s' % str(list(supported_methods.keys())))
 	args = parser.parse_args()
 
 	dataset = args.dataset[0]
@@ -71,6 +88,6 @@ if __name__ == "__main__":
 	print('-- Loading %s data' % dataset)
 	data, colors = generate_data(dataset)
 	print('-- Embedding %s data with %s' % (dataset,method))
-	embedded_data = embed(data,method)
+	embedded_data = embed(data, method)
 	print('-- Plotting embedded data')
-	plot(data,embedded_data,colors)
+	plot(data, embedded_data, colors, supported_methods[method])
