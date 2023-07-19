fdc99dffc9820259dd6a17db148be585a9be14b5
Sergey Lisitsyn <lisitsyn@hey.com>
2023-07-19 16:29:09 +0100

    Fix Varioqub

--- src/tapkee/core.clj
+++ src/tapkee/core.clj
@@ -79,14 +79,11 @@
 
 (defn varioqub []
   [:script """
-    <!-- Varioqub experiments -->
-  <script type=\"text/javascript\">
   (function(e, x, pe, r, i, me, nt){
   e[i]=e[i]||function(){(e[i].a=e[i].a||[]).push(arguments)},
   me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],nt.parentNode.insertBefore(me,nt)})
   (window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab');
   ymab('metrika.64440154', 'init'/*, {clientFeatures}, {callback}*/);
-  </script>
    """])
 
 (defn forkme []
