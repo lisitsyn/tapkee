c0c73e5d2ce7accc124e71aa79afcbb61f94aeff
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-05-11 20:00:12 +0200

    Finish PoC.

--- nanobind.py
+++ nanobind.py
@@ -6,13 +6,20 @@ import numpy as np
 def parse_reduction_method(method_name: str):
     return tapkee.parse_reduction_method(method_name)
 
-if __name__=="__main__":
-    parameters = tapkee.ParametersSet()
-    method = parse_reduction_method('ra')
-    assert(method.name == 'Random Projection')
+def test_exception_unknown_method():
     try:
         parse_reduction_method('unknown')
         assert(False)
     except:
         pass
-    #tapkee.initialize().withParameters(parameters).embedUsing(np.array([[0,2],[3,1]]))
+
+if __name__=='__main__':
+    test_exception_unknown_method()
+    parameters = tapkee.ParametersSet()
+    method = parse_reduction_method('ra')
+    assert(method.name == 'Random Projection')
+    parameters.add(tapkee.Parameter.create('dimension reduction method', method))
+    tapkee_input  = np.array([[0,2,4],[3,1,9],[5,7,6]])
+    tapkee_output = tapkee.initialize().withParameters(parameters).embedUsing(tapkee_input)
+    print('>> From:\n', tapkee_input);
+    print('<< To:\n', tapkee_output.embedding)
