c129206438f31a05d3735eca0e35cfef470e03ba
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-05-11 20:00:12 +0200

    Finish PoC.

--- src/python/nanobind_extension.cpp
+++ src/python/nanobind_extension.cpp
@@ -1,28 +1,25 @@
-#include <string>
+#include <cli/util.hpp>                // for parse_reduction_method, TODO consider moving from src to include
 
 #include <stichwort/parameter.hpp>     // for Parameter::create
 
 #include <tapkee/chain_interface.hpp>  // for initialize, withParameters
 #include <tapkee/defines.hpp>          // for ParametersSet, TapkeeOutput
 #include <tapkee/defines/methods.hpp>  // for DimensionReductionMethod
-#include <tapkee/defines/types.hpp>    // for DenseMatrix
-
-#include <cli/util.hpp> // TODO consider moving to include
 
 #include <nanobind/nanobind.h>
-#include <nanobind/eigen/dense.h>
+#include <nanobind/eigen/dense.h>      // for type caster embedUsing
+#include <nanobind/stl/string.h>       // for type caster calling stichwort::Parameter::create(const std::string&, ...
 
 namespace nb = nanobind;
 
 using stichwort::Parameter;
 
 using tapkee::initialize;
-// using tapkee::DenseMatrix;  // superseded by typecaster in nanobind/eigen/dense.h
 using tapkee::DimensionReductionMethod;
 using tapkee::ParametersSet;
 using tapkee::TapkeeOutput;
 
-using tapkee::tapkee_internal::ParametersInitializedState;  // externalize in tapkee?
+using tapkee::tapkee_internal::ParametersInitializedState;  // TODO consider making it part of the "external" API
 
 NB_MODULE(tapkee, m) {
     nb::class_<initialize>(m, "initialize")
@@ -30,15 +27,15 @@ NB_MODULE(tapkee, m) {
         .def("withParameters", &initialize::withParameters);
 
     nb::class_<ParametersSet>(m, "ParametersSet")
-        .def(nb::init<>());
+        .def(nb::init<>())
+        .def("add", &ParametersSet::add);
 
     nb::class_<ParametersInitializedState>(m, "ParametersInitializedState")
         .def(nb::init<const ParametersSet&>())
-        // TapkeeOutput embedUsing(const DenseMatrix& matrix) const
         .def("embedUsing", &ParametersInitializedState::embedUsing);
 
-    //nb::class_<DenseMatrix>(m, "DenseMatrix");  // compilation error together w/ type caster
-    nb::class_<TapkeeOutput>(m, "TapkeeOutput");
+    nb::class_<TapkeeOutput>(m, "TapkeeOutput")
+        .def_rw("embedding", &TapkeeOutput::embedding);
 
     m.def("parse_reduction_method", &parse_reduction_method);
 
@@ -46,6 +43,5 @@ NB_MODULE(tapkee, m) {
         .def_rw("name", &DimensionReductionMethod::name_);
 
     nb::class_<Parameter>(m, "Parameter")
-        .def("create", &Parameter::create<int>)
-        .def("create", &Parameter::create<std::string>);
+        .def_static("create", &Parameter::create<DimensionReductionMethod>);
 }
