d60b9dc9fe0df8a54a037f8a265631470f0cfa82
Fernando J. Iglesias García <fernando.iglesiasg@gmail.com>
2024-05-24 10:38:36 +0200

    Replace includes and pi in unit test. (#105)

--- test/unit/data.hpp
+++ test/unit/data.hpp
@@ -1,14 +1,11 @@
-#include <cmath>
+#include <tapkee/defines/types.hpp>
 
-#include <fstream>
-
-#ifndef M_PI
-#define M_PI (3.14159265358979323846)
-#endif
+#include <numbers>
 
 tapkee::DenseMatrix swissroll(int N)
 {
-    tapkee::DenseVector tt = (3.0 * M_PI / 4.0) * (tapkee::DenseVector::Random(N).array() + 0.5);
+    tapkee::DenseVector tt = (3.0 * std::numbers::pi_v<double> / 4.0) *
+                              (tapkee::DenseVector::Random(N).array() + 0.5);
     tapkee::DenseVector height = tapkee::DenseVector::Random(N).array() - 0.5;
     tapkee::DenseMatrix X(N, 3);
     X.col(0) = tt.array() * tt.array().cos();
